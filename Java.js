5380c6d4a42c4724f6056a745800db08f28548b1
Lukej04 <138320702+Lukej04@users.noreply.github.com>
2023-07-08 22:46:45 -0400

    Update Java.js

--- Java.js
+++ Java.js
@@ -5,6 +5,7 @@ function openNav() {
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
+
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
