54a02d5709f99f9adf62a9a8820430adf1f5c292
Lukej04 <138320702+Lukej04@users.noreply.github.com>
2023-07-09 22:32:34 -0400

    Update Successful-bypass.js

--- Successful-bypass.js
+++ Successful-bypass.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("click", function() {
   const form = document.getElementById("my-form");
   form.addEventListener("submit", function(e) {
     e.preventDefault();
